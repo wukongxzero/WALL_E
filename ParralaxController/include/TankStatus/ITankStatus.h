c85db746241b5974995112ac0135668232e7949c
jordanirgang <jordanirgang@gmail.com>
2026-03-15 13:47:02 -0700

    Telemetry Headers templated for Tank Status

--- ParralaxController/include/TankStatus/ITankStatus.h
+++ ParralaxController/include/TankStatus/ITankStatus.h
@@ -1,3 +1,5 @@
+#pragma once
+
 // todo: rename file as from ITankStatus to tankStatus
 struct TankStatus {
   volatile int driveLeft;
@@ -7,3 +9,9 @@ struct TankStatus {
   volatile int eulerY;
   volatile int eulerZ;
 };
+
+void makeByteTankStatus(unsigned char *buffer, int byteLength,
+                        struct TankStatus *ts);
+
+void readByteTankeStatus(unsigned char *buffer, int byteLength,
+                         struct TankStatus *ts);
