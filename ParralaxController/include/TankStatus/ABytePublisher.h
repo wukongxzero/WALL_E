f2e767ba7902a66fba2e7e7e9ae693edefd1182b
jordanirgang <jordanirgang@gmail.com>
2026-03-16 10:30:17 -0700

    updated oop to be functional with psudo object; for parralax telemetry

--- ParralaxController/include/TankStatus/ABytePublisher.h
+++ ParralaxController/include/TankStatus/ABytePublisher.h
@@ -1,18 +1,32 @@
-#pragma once
-#include "IOutImpl.h"
-#include "TankStatus/ATankStatusPublisher.h"
 
-class ABytePublisher : public ATankStatusPublisher {
-public:
-  IOutImpl byteSender;
+#ifndef USART_BYTE_PUBLISHER
+#define USART_BYTE_PUBLISHER
 
-#ifdef VIRTUALDEFINED
-  virtual void listen();
-  virtual void write();
-  virtual void connect();
-#else
-  void listen();
-  void write();
-  void connect();
-#endif
+#include "ATankStatusPublisher.h"
+#include "BTUSART.h"
+#include "TankStatus.h"
+#include <simpletools.h>
+// BT hc-06
+// https://www.parallax.com/multiple-serial-port-16-object/
+struct UsartBytePublisher {
+  // TODO: add USART Hand
+  // putchar and getchar are used to call placement of char into usart(pin 31)
+  PropellorUSART *btInterface;
+  // loop over TankStatusPublisher->TankStatisSubscriberList to notify all
+  // subscriber
+  TankStatusPublisher *ptrWithSubscriberList;
+
+  // store serial read usart data into this struct which will be used to notify
+  TankStatus buffer;
 };
+
+// TODO:take in a message Struct refrence and update all the
+// TankStatusSubscribers' TanksStatus pointers to update to the messageStruct
+// input
+void notify(UsartBytePublisher *self, TankStatus &messageStruct);
+
+// TODO:Update the tankstatus buffer using the readByteTankStatus() method after
+// gathering getchar() data from the btInterface
+void readInterface(UsartBytePublisher *self);
+
+#endif
---
+++ ParralaxController/include/TankStatus/TankStatus.h
@@ -0,0 +1,28 @@
+#ifndef TANKSTATUS_SYNC_STRUCT
+#define TANKSTATUS_SYNC_STRUCT
+
+#define TANKSTATUS_PACKET_LENGTH                                               \
+  16 // bytes,1 +1+4+4+4 + 2 bits for extra room/ keep as multiple of 8
+
+// todo: rename file as from ITankStatus to tankStatus
+struct TankStatus {
+  volatile unsigned char driveLeft;  // 1 byte 0-255
+  volatile unsigned char driveRight; // 1 byte 0-255
+
+  // TODO: can optimize float into unsigned decimal point char and a signed char
+  // for the non decimal portion range would be +/- 128.(0-255) degrees/radians
+  // and only transmit as 2 bytes
+  // radians would be more compressed for bit streamuing
+  volatile float eulerX; // 4 byte 32 bit
+  volatile float eulerY; // 4 byte
+  volatile float eulerZ; // 4 byte
+};
+// TODO: should turn all chars into one char byte array as a buffer to be
+// streamed over usart
+void makeByteTankStatus(unsigned char *buffer, int byteLength,
+                        struct TankStatus *ts);
+
+// TODO: should take a buffer array and package it into a TankStatus structure
+void readByteTankStatus(unsigned char *buffer, int byteLength,
+                        struct TankStatus *ts);
+#endif
