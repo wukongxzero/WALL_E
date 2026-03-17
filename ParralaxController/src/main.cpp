85f0738b448b86942f75f55b79e934492f3d87f6
jordanirgang <jordanirgang@gmail.com>
2026-03-16 18:21:33 -0700

    funcky git stuff

--- ParralaxController/src/main.cpp
+++ ParralaxController/src/main.cpp
@@ -1,12 +1,42 @@
+<<<<<<< Updated upstream
 #include <Graphics/ARendederSubscriber.h>
 #include <Propellor/SPI.h>
 #include <TankStatus/ABytePublisher.h>
 #include <propeller.h>
 #include <simpletools.h>
+=======
+
+#include <TankStatus/ABytePublisher.h>
+#include <TankStatus/IOutImpl.h>
+#include <TankStatus/ITankStatus.h>
+
+>>>>>>> Stashed changes
 #include <stdio.h>
 
+#define SIMULATION_SCREEN
+
+#ifndef SIMULATION_SCREEN
+#include "simpletools.h"
+#include <propeller.h>
 int main(void) {
   for (;;) {
   }
   return 0;
 }
+#else
+#include <Graphics/NcursesScreenImpl.h>
+#include <ncurses.h>
+int main(void) {
+  initscr();
+  noecho();
+  nocbreak();
+  keypad(stdscr, TRUE);
+
+  draw_rectangle(5, 10, 15, 40);
+  mvwprintw(stdscr, 10, 10, "hello curse");
+  refresh();
+  getch();
+  endwin();
+}
+
+#endif
